43267d66f9e8e9f7dbbc033b0e549cf70c7cf22f
Yingte Xu <lucianoxu@foxmail.com>
2022-08-09 17:00:06 +0800

    lexer and pharser completed. semantics check next.

--- lexer.py
+++ lexer.py
@@ -13,33 +13,43 @@ reserved = {
     'else': 'ELSE',
     'while': 'WHILE',
     'do' : 'DO',
-    'end' : 'END'
+    'end' : 'END',
 }
 
 # List of token names.
 tokens = [
-    'ID',   #identity
+    'ID',
     'ASSIGN',
-    'UNITARY',
-    'SEQUENCE',
+    'ZERO',
+    'MUL_EQ',
+    'SEMICOLON',
     'NONDET_CHOICE',
     'LSBRAKET',
-    'RSBRAKET'
+    'RSBRAKET',
+    'LBRACE',
+    'RBRACE'
  ] + list(reserved.values())
 
 # Regular expression rules for simple tokens
 t_ASSIGN = r':='
-t_UNITARY = r'\*='
-t_SEQUENCE = r';'
+t_ZERO = r'0'
+t_MUL_EQ = r'\*='
+t_SEMICOLON = r';'
 t_NONDET_CHOICE = r'[#]'
 t_LSBRAKET = r'\['
 t_RSBRAKET = r'\]'
+t_LBRACE = r'{'
+t_RBRACE = r'}'
+
+# use // to comment a line
+def t_COMMENT(t):
+    r'//.*'
+    pass
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'ID')    # Check for reserved words
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
     return t
-
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -54,24 +64,10 @@ t_ignore = ' \t'
 
 
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    print("Illegal character '" + t.value[0] + "' at line " + str(t.lineno))
     t.lexer.skip(1)
+
 
 
 # Build the lexer
 lexer = lex.lex()
-
-# Test it out
-data = '''
-ifandonlyif if if] while [#] jnj;end
-'''
-
-# Give the lexer some input
-lexer.input(data)
-
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok:
-        break      # No more input
-    print(tok)
---
+++ parser.py
@@ -0,0 +1,84 @@
+import ply.yacc as yacc
+
+from lexer import tokens, lexer
+
+# program declaration section
+
+def p_sequence_form(p):
+    'sequence : sentence'
+    p[0] = [p[1]]
+
+def p_sequence_append(p):
+    'sequence : sequence SEMICOLON sentence'
+    p[0] = p[1] + [p[3]]
+
+
+def p_sentence(p):
+    '''
+    sentence : skip
+        | abort
+        | init
+        | unitary
+        | if
+        | while
+        | nondet_choice
+    '''
+    p[0] = p[1]
+
+def p_nondet_choice(p):
+    'nondet_choice : LBRACE sequence NONDET_CHOICE sequence RBRACE'
+    p[0] = ('nondet_choice', p[2], p[4])
+
+def p_if(p):
+    'if : IF ID id_ls THEN sequence ELSE sequence END'
+    p[0] = ('IF', p[2], p[3], p[5], p[7])
+
+def p_while(p):
+    'while : WHILE ID id_ls DO sequence END'
+    p[0] = ('WHILE', p[2], p[3], p[5])
+
+
+def p_skip(p):
+    'skip : SKIP'
+    p[0] = ('SKIP',)
+
+def p_abort(p):
+    'abort : ABORT'
+    p[0] = ('ABORT',)
+
+def p_unitary(p):
+    '''unitary : id_ls MUL_EQ ID
+                | ID MUL_EQ ID'''
+    if isinstance(p[1], list):
+        p[0] = ('UNITARY', p[1], p[3])
+    else:
+        p[0] = ('UNITARY', [p[1]], p[3])
+
+def p_init(p):
+    '''init : id_ls ASSIGN ZERO
+            | ID ASSIGN ZERO'''
+    if isinstance(p[1], list): 
+        p[0] = ('INIT', p[1])
+    else:
+        p[0] = ('INIT', [p[1]])
+
+# define the list of quantum variables
+def p_vars_end(p):
+    'id_ls : id_ls_pre RSBRAKET'
+    p[0] = p[1]
+
+def p_vars_append(p):
+    'id_ls_pre : id_ls_pre ID'
+    p[0] = p[1] + [p[2]]
+
+def p_vars_start(p):
+    'id_ls_pre : LSBRAKET ID'
+    p[0] = [p[2]]
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input: '" + str(p.value) + "' " + str(p))
+
+
+# Build the lexer
+parser = yacc.yacc()
